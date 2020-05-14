db5b9963f46bd209619d12147e2497490d77a7b3
unknown <svgrgr@gmail.com>
2020-05-14 19:11:49 +0300

    add gulp task: minimize css and rename with .min

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
 
+var cssmin = require("gulp-cssmin");
+var rename = require("gulp-rename");
+
 gulp.task("hello", function (done) {
   console.log("Привет, мир!");
   done();
@@ -14,4 +17,13 @@ gulp.task("browser-sync", function () {
     },
   });
   gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+//Minimize and rename .css with suffix "min"
+gulp.task("default", function () {
+  gulp
+    .src("src/**/*.css")
+    .pipe(cssmin())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist"));
 });
