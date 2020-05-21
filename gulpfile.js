f81ad45e29c4913213283a5d4e5cbb9e5720a9f4
svgrgr <62762871+svgrgr@users.noreply.github.com>
2020-05-21 08:39:22 +0300

    Added gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const { src, dest, watch } = require("gulp");
 const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
+const gulp = require('gulp');//for autoprefixer
+const autoprefixer = require('gulp-autoprefixer');//for autoprefixer
 
 //var cssmin = require("gulp-cssmin");
 //var rename = require("gulp-rename");
@@ -18,12 +20,23 @@ function bs() {
   watch("./js/*.js").on("change", browserSync.reload);
 }
 
+//sass-to-css
 function serveSass() {
   return src("./sass/*.sass")
     .pipe(sass())
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
 }
+
+//autoprefixer
+exports.default = () => (
+  gulp.src('src/app.css')
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist'))
+);
+//autoprefixer
 
 exports.serve = bs;
 
