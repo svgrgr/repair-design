6c28c17378af13cdf585620dd98bb832376678b5
svgrgr <62762871+svgrgr@users.noreply.github.com>
2020-05-31 00:52:41 +0300

    feat: added js function for modal

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal_visible');
+  }
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+
+});
