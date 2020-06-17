e0cfc2cae680396e3777b1c4e7c17bc6541f3bbe
svgrgr <62762871+svgrgr@users.noreply.github.com>
2020-06-17 23:59:53 +0300

    forms adaptives done

--- send.php
+++ send.php
@@ -15,12 +15,13 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 try {
     //Server settings
+    $mail->CharSet = 'UTF-8';
     $mail->SMTPDebug = 0;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'ivaninkin89@gmail.com';                     // SMTP username
-    $mail->Password   = 'polikol1!';                               // SMTP password
+    $mail->Password   = 'crispysnow';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
